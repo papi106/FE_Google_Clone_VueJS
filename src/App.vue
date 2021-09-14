da85bbf698d60d986b55065535d1b45429ec2b65
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-14 18:31:20 +0300

    Created the needed files for the app Added images

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
