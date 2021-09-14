3ad37c748a19c1bf5dc2c58029bd93af9e76e915
Patrick Pacurar <patrickpacurar@yahoo.com>
2021-09-14 18:45:21 +0300

    Added header part for google Added the whole CSS file from last project Small changes in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
-
+<Header></Header>
 </template>
 
 <script>
-
+import Header from '.components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+    Footer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+      <nav class="nav">
+        <ul class="navlinks">
+          <li><a href="#" class="navlink">Gmail</a></li>
+          <li><a href="#" class="navlink">Imagini</a></li>
+        </ul>
+        <div class="apps">
+          <img src="./images/apps.svg" />
+        </div>
+        <div class="me" >
+          <img src="/images/me.png" style="border-radius: 50%;"/>
+        </div>
+      </nav>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style>
+@import '../assets/styles/main.css';
+</style>
